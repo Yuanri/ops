cbe3ebdad5c7359136e8c5b6338cd6d4ef694308
Yuani <osmer@sina.com>
2017-11-22 14:21:51 +0800

    Update denyhosts_install.sh

--- Security/denyhosts_install.sh
+++ Security/denyhosts_install.sh
@@ -1,48 +1,12 @@
-#!/usr/bin/bash
+#!/bin/bash
+# denyhosts install
 #
-# name: denyhosts_install.sh
-#
-# denyhosts install scripts
 
-URL=https://github.com/denyhosts/denyhosts/archive/master.zip
-
-
-MakeSureSecuFile(){
-    SecuFile=/var/log/messages
-    test $(cat /etc/issue |awk 'NR==1{print $1}') == 'CentOS' && SecuFile=/var/log/secure
-    if test -z $(ldd $(which sshd)|grep libwrap) ;then
-        SecuFile=/var/log/messages
-        echo 'The Current SSHD is not soupport wrap'
-        echo 'Set iptables plugin script '
-        sed -i "s@#IPTABLES.*@IPTABLES = $(which iptables)@"  /etc/denyhosts.conf
-        sed -i "s@#PLUGIN_DENY=.*@PLUGIN_DENY=/var/lib/denyhosts/plugins/iptablesAddIp.sh@" /etc/denyhosts.conf
-        sed -i "s@#PLUGIN_PURGE=.*@PLUGIN_PURGE=/var/lib/denyhosts/plugins/iptablesRemoveIp.sh@" /etc/denyhosts.conf
-        test -d plugins  && mv  plugins /var/lib/denyhosts/
-    fi
-
-    sed -i "s#^SECURE_LOG.*#SECURE_LOG = $SecuFile#" /etc/denyhosts.conf
-}
-
-echo "Begin to download denyhosts to System ...."
-wget  $URL
-
-echo "Install denyhost ..."
-if test -f $PageName ;then
-        unzip  master.zip 
-        cd denyhosts-master
-        pip install -r requirements.txt
-        python setup.py install
-        echo "Install Successed"
-        echo "The fist time to exec  denyhosts .... "
-        MakeSureSecuFile
-        /usr/bin/denyhosts.py
-        sleep 5
-        echo " download witelist and logrorate config"
-       
-        echo "Please modify the witeList file: /var/lib/denyhosts/allowed-hosts
+URL_denyhosts=https://github.com/denyhosts/denyhosts/archive/master.zip
+URL_ipaddr=https://pypi.python.org/packages/9d/a7/1b39a16cb90dfe491f57e1cab3103a15d4e8dd9a150872744f531b1106c1/ipaddr-2.2.0.tar.gz
         
-        # config logrorate
-        echo '/var/log/denyhosts {
+# config logrorate
+echo '/var/log/denyhosts {
 	copytruncate
 	daily
 	dateext
@@ -51,13 +15,51 @@ if test -f $PageName ;then
 	notifempty
 	rotate 7
 }'   >  /etc/logrotate.d/denyhosts 
-       
-        echo "Set daemo progress : ln -s /usr/bin/daemon-control-dist /etc/init.d/denyhosts "
-        ln -s /usr/bin/daemon-control-dist /etc/init.d/denyhosts
-        ln -s /usr/bin/denyhosts.py  /usr/sbin/denyhosts
-        chkconfig --level 2345 denyhosts on
-        
-        echo 'the  Command to start denyhosts as a daemon process: /etc/init.d/denyhosts start'
-else
-        echo "ERR: Download file fail ,URL:$URL "
+
+
+#install ipaddr
+wget $URL_ipaddr
+tar -zxf ipaddr-2.2.0.tar.gz
+cd  ipaddr-2.2.0
+python setup.py install
+cd ..
+
+# download denyhosts.tgz and install
+wget $URL_denyhosts
+unzip  master.zip
+cd  denyhosts-master
+python setup.py install
+
+
+# set config
+mkdir -p /var/lib/denyhosts
+cp -r  allowed-hosts  plugins  /var/lib/denyhosts/
+cp  logrorate-denyhosts  /etc/logrotate.d/
+echo '/etc/init.d/denyhosts start' >> /etc/rc.local
+
+echo "=== copy the daemon script "
+echo "cp  daemon-control-dist /etc/init.d/denyhosts"
+echo "cp  denyhosts.py  /usr/sbin/denyhosts"
+cp  daemon-control-dist /etc/init.d/denyhosts
+cp  denyhosts.py  /usr/sbin/denyhosts
+
+# check sshd neither support the  tcp_wrap module
+if test -z $(ldd $(which sshd) | awk '/wrap/{print "ok"}')
+then
+	echo '=== The Current SSHD is not support wrap ===='
+        echo '=== Set iptables plugin script ==============='
+        sed -i "s@#PLUGIN_DENY=.*@PLUGIN_DENY=/var/lib/denyhosts/plugins/iptablesAddIp.sh@" /etc/denyhosts.conf
+        sed -i "s@#PLUGIN_PURGE=.*@PLUGIN_PURGE=/var/lib/denyhosts/plugins/iptablesRemoveIp.sh@" /etc/denyhosts.conf
+	
+	#若设置PLUGIN, 需要下载iptablesAddIp.sh  iptablesRemoveIp.sh
 fi
+
+# set the secure file
+SecuFile=/var/log/secure
+test -f $SecuFile ||  SecuFile=/var/log/auth.log
+echo "=== Secure File: $SecuFile"
+sed -i "s#^SECURE_LOG.*#SECURE_LOG = $SecuFile#" /etc/denyhosts.conf
+tail -n20 $SecuFile
+
+echo "=== Start denyhosts ... "
+/etc/init.d/denyhosts start
